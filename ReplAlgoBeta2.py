3fc55e9f55e80b6e6510aeffa559814f6adc8bd2
Puru Pathak <puruppathak@gmail.com>
2017-06-07 11:24:42 -0400

    Now takes the last 3 characters in the Org ID string and searches with Drug name

---
+++ ReplAlgoBeta2.py
@@ -0,0 +1,472 @@
+__author__ = 'puruppathak'
+
+import csv
+from itertools import filterfalse, zip_longest
+import csv
+from random import choice
+import sys
+import bs4
+import lxml
+import urllib.request
+from itertools import islice
+from urllib.request import Request, urlopen
+from urllib.error import URLError
+import xml.etree.ElementTree as ET
+import re
+alist=[]
+alist2=[]
+
+OrgID=[]
+FDAOrg1=[]
+NCTID=[]
+Btitle=[]
+OverallStatus=[]
+condition=[]
+agency=[]
+gender=[]
+MAge=[]
+Phase1=[]
+AgencyClass=[]
+StudyType=[]
+StudyDesign=[]
+SecID=[]
+FirstRecv=[]
+SDate=[]
+ComplDate=[]
+LastChangedDate=[]
+VerificationDate=[]
+FirstRecvResultDate=[]
+PCD=[]
+Countries=[]
+Condition=[]
+IsFDA=[]
+Is801=[]
+Intervention=[]
+acronym=[]
+alist2=[]
+FDAOrg=[]
+Rm_Countries=[]
+FirstRecvResultDispoDate=[]
+
+
+list1 = ['P06042','P101','19.4.306','19.4.334','P07981','P105','19.4.301','19.4.302','19.4.310','19.4.303','P07982','19.4.110','19.4.112','19.4.113','19.4.114','19.4.115','19.4.116','19.4.313','19.4.316','19.4.318','19.4.324','P06101','19.4.328','19.4.333','19.4.335','P07025','P07044','P07038','19.4.319','19.4.101','19.4.102','19.4.105','19.4.106','19.4.107','19.4.108','19.4.109','19.4.201','19.4.202','19.4.203','19.4.204','19.4.205','19.4.206','19.4.207','19.4.208A','19.4.208B','19.4.209A','19.4.209B','19.4.210','19.4.304','19.4.305','19.4.308','19.4.309','19.4.311','19.4.312']
+Cleaned = []
+
+drugname = input("Enter the Drug name ")
+list1 = [x.strip(' ') for x in list1]
+len1 = len(list1)-1
+alist = [];
+strURL1 = 'https://clinicaltrials.gov/ct2/results?term=%22'
+strURL2 = '%22+AND+%22'
+strURL3 = '%22+&Search=Searchdisplayxml=true'
+
+for word in list1:
+   newstring = word[-3:]
+   Cleaned.append(newstring)
+
+print(Cleaned)
+
+
+for indic1 in Cleaned:
+
+
+  strURL4 = strURL1 + drugname + strURL2 + indic1 + strURL3
+#req = urllib.request.urlopen('https://clinicaltrials.gov/show/NCT01487096?displayxml=true')
+# https://clinicaltrials.gov/ct2/results?term=%22Aubagio%22+AND+%222000%22+&Search=Searchdisplayxml=true
+
+
+
+  try:
+    req = urllib.request.urlopen(strURL4)
+    xml = bs4.BeautifulSoup(req, 'xml')
+    for item in xml.findAll('search_results'):
+     y=re.findall('<nct_id>(.*)</nct_id>',str(item))
+     len2 = len(y)
+     if y:
+      alist.append(y)
+      l = [indic1] * len2
+      FDAOrg1.append(l)
+      #FDAOrg.append(indic1)
+     else:
+         y1=re.findall('<query>[^ ]* (.*)</query>',str(item))
+         if y1:
+          alist2.append(y1)
+     #print ("NCT IS: "+item.text)
+    #for i in range(0,len1):
+     #alist.append(item)
+  except URLError as e:
+    if hasattr(e, 'reason'):
+        print('We failed to reach a server.')
+        print('Reason: ', e.reason)
+    elif hasattr(e, 'code'):
+        print('The server couldn\'t fulfill the request.')
+        print('Error code: ', e.code)
+  else:
+
+        print("Stage 1 processing ...")
+
+
+
+
+final3=sum(FDAOrg1, [])
+final2=sum(alist2, [])
+
+
+
+final=sum(alist, [])
+print(final)
+set = set(final)
+result = list(set)
+
+strURL11 = 'https://clinicaltrials.gov/show/'
+strURL21 = '?displayxml=true'
+
+for indic11 in final:
+ strURL31 = strURL11 + indic11 + strURL21
+
+
+ try:
+
+
+    req2 = urllib.request.urlopen(strURL31)
+    xml = bs4.BeautifulSoup(req2, 'xml')
+    for item in xml.findAll('id_info'):
+       party = item.find('org_study_id')
+       if party is None:
+        OrgID.append("NULL")
+       else:
+        NewItem = party.text
+
+    #for i in range(0,len1):
+        OrgID.append(NewItem)
+    for item in xml.findAll('id_info'):
+      party = item.find('nct_id')
+      if party is None:
+        NCTID.append("NULL")
+      else:
+         NewItem = party.text
+
+        #for i in range(0,len1):
+         NCTID.append(NewItem)
+
+    for item in xml.findAll('clinical_study'):
+     party = item.find('brief_title')
+     if party is None:
+      Btitle.append("NULL")
+     else:
+      NewItem = party.text
+
+    #for i in range(0,len1):
+      Btitle.append(NewItem)
+
+
+    for item in xml.findAll('clinical_study'):
+     party = item.find('overall_status')
+     if party is None:
+      OverallStatus.append("NULL")
+     else:
+      NewItem = party.text
+
+    #for i in range(0,len1):
+      OverallStatus.append(NewItem)
+
+
+    for item in xml.findAll('clinical_study'):
+     party = item.find('condition')
+     if party is None:
+      condition.append("NULL")
+     else:
+      NewItem = party.text
+
+    #for i in range(0,len1):
+      condition.append(NewItem)
+
+
+     for item in xml.findAll('lead_sponsor'):
+      party = item.find('agency')
+      if party is None:
+       agency.append("NULL")
+      else:
+       NewItem = party.text
+
+    #for i in range(0,len1):
+       agency.append(NewItem)
+
+
+     for item in xml.findAll('eligibility'):
+      party = item.find('gender')
+      if party is None:
+       gender.append("NULL")
+      else:
+       NewItem = party.text
+
+    #for i in range(0,len1):
+       gender.append(NewItem)
+
+
+
+
+     for item in xml.findAll('eligibility'):
+      party = item.find('minimum_age')
+      if party is None:
+       MAge.append("NULL")
+      else:
+       NewItem = party.text
+
+    #for i in range(0,len1):
+       MAge.append(NewItem)
+
+
+
+     for item in xml.findAll('clinical_study'):
+      party = item.find('phase')
+      if party is None:
+       Phase1.append("NULL")
+      else:
+       NewItem = party.text
+
+    #for i in range(0,len1):
+       Phase1.append(NewItem)
+
+
+
+     for item in xml.findAll('lead_sponsor'):
+      party = item.find('agency_class')
+      if party is None:
+       AgencyClass.append("NULL")
+      else:
+       NewItem = party.text
+
+    #for i in range(0,len1):
+       AgencyClass.append(NewItem)
+
+
+     for item in xml.findAll('clinical_study'):
+      party = item.find('study_type')
+      if party is None:
+       StudyType.append("NULL")
+      else:
+       NewItem = party.text
+       StudyType.append(NewItem)
+
+
+
+
+     for item in xml.findAll('clinical_study'):
+      party = item.find('study_design')
+      if party is None:
+       StudyDesign.append("NULL")
+      else:
+       NewItem = party.text
+       StudyDesign.append(NewItem)
+
+
+     for item in xml.findAll('id_info'):
+      party = item.find('secondary_id')
+      if party is None:
+       SecID.append("NULL")
+      else:
+       NewItem = party.text
+
+    #for i in range(0,len1):
+       SecID.append(NewItem)
+
+
+
+     for item in xml.findAll('clinical_study'):
+      party = item.find('firstreceived_date')
+      if party is None:
+       FirstRecv.append("NULL")
+      else:
+       NewItem = party.text
+
+    #for i in range(0,len1):
+       FirstRecv.append(NewItem)
+
+     for item in xml.findAll('clinical_study'):
+      party = item.find('start_date')
+      if party is None:
+       SDate.append("NULL")
+      else:
+       NewItem = party.text
+
+    #for i in range(0,len1):
+       SDate.append(NewItem)
+
+
+    for item in xml.findAll('clinical_study'):
+     party = item.find('completion_date')
+     if party is None:
+      ComplDate.append("NULL")
+     else:
+      NewItem = party.text
+
+    #for i in range(0,len1):
+      ComplDate.append(NewItem)
+
+
+
+    for item in xml.findAll('clinical_study'):
+     party = item.find('lastchanged_date')
+     if party is None:
+      LastChangedDate.append("NULL")
+     else:
+      NewItem = party.text
+
+    #for i in range(0,len1):
+      LastChangedDate.append(NewItem)
+
+
+
+    for item in xml.findAll('clinical_study'):
+     party = item.find('verification_date')
+     if party is None:
+      VerificationDate.append("NULL")
+     else:
+      NewItem = party.text
+
+    #for i in range(0,len1):
+      VerificationDate.append(NewItem)
+
+    for item in xml.findAll('clinical_study'):
+     party = item.find('firstreceived_results_date')
+     if party is None:
+      FirstRecvResultDate.append("NULL")
+     else:
+      NewItem = party.text
+
+    #for i in range(0,len1):
+      FirstRecvResultDate.append(NewItem)
+
+    for item in xml.findAll('clinical_study'):
+     party = item.find('firstreceived_results_disposition_date')
+     if party is None:
+      FirstRecvResultDispoDate.append("NULL")
+     else:
+      NewItem = party.text
+
+    #for i in range(0,len1):
+      FirstRecvResultDispoDate.append(NewItem)
+
+
+
+
+    for item in xml.findAll('clinical_study'):
+     party = item.find('primary_completion_date')
+     if party is None:
+      PCD.append("NULL")
+     else:
+      NewItem = party.text
+
+    #for i in range(0,len1):
+      PCD.append(NewItem)
+
+
+
+    for item in xml.findAll('clinical_study'):
+     party = item.find('location_countries')
+     if party is None:
+      Countries.append("NULL")
+     else:
+      NewItem = party.text
+
+    #for i in range(0,len1):
+      Countries.append(NewItem)
+
+
+    for item in xml.findAll('clinical_study'):
+     party = item.find('removed_countries')
+     if party is None:
+      Rm_Countries.append("NULL")
+     else:
+      NewItem = party.text
+
+    #for i in range(0,len1):
+      Rm_Countries.append(NewItem)
+
+
+
+    for item in xml.findAll('clinical_study'):
+     party = item.find('condition')
+     if party is None:
+      Condition.append("NULL")
+     else:
+      NewItem = party.text
+
+    #for i in range(0,len1):
+      Condition.append(NewItem)
+
+
+
+    for item in xml.findAll('clinical_study'):
+     party = item.find('is_fda_regulated')
+     if party is None:
+      IsFDA.append("NULL")
+     else:
+      NewItem = party.text
+
+    #for i in range(0,len1):
+      IsFDA.append(NewItem)
+
+    for item in xml.findAll('clinical_study'):
+     party = item.find('acronym')
+     if party is None:
+      acronym.append("NULL")
+     else:
+      NewItem = party.text
+
+    #for i in range(0,len1):
+      acronym.append(NewItem)
+
+
+
+    for item in xml.findAll('clinical_study'):
+     party = item.find('is_section_801')
+     if party is None:
+      Is801.append("NULL")
+     else:
+      NewItem = party.text
+
+    #for i in range(0,len1):
+      Is801.append(NewItem)
+
+    for item in xml.findAll('clinical_study'):
+       y=re.findall('<intervention_name>(.*)</intervention_name>',str(item))
+       if y:
+        Intervention.append(y)
+       else:
+        Intervention.append("NULL")
+
+
+
+ except URLError as e:
+      if hasattr(e, 'reason'):
+        print('We failed to reach a server.')
+        print('Reason: ', e.reason)
+      elif hasattr(e, 'code'):
+        print('The server couldn\'t fulfill the request.')
+        print('Error code: ', e.code)
+ else:
+
+
+
+         print("Stage 2 processing..")
+
+
+
+print(FDAOrg)
+f = open('Full_temp_data.csv', 'wt')
+try:
+    writer = csv.writer(f)
+    writer.writerow( ('FDA Org ID','Org ID', 'NCT ID','Brief Title','Condition','Overall status','Intervention name','Acronym','Agency','Gender','Minimum Age','Phase','Agency class','Study type','Study Design','Secondary ID','First received','Start Date','Completion date','Last changed date','Verification date','First received result date','Certificate of Delay/ Disposition Date','PCD','Countries','Removed countries','IS FDA regulated','IS section 801'))
+    #mylist = list(set(list01))
+    #mylist1 = list(set(list02))
+    #mylist2 = list(set(list03))
+    #for i1 in list11:
+    for i1,i2,i3,i4,i5,i6,i7,i8,i9,i10,i11,i12,i13,i14,i15,i16,i17,i18,i19,i20, i21, i22, i23,i24, i25, i26, i27,i28 in zip(final3, OrgID, NCTID, Btitle,Condition, OverallStatus,Intervention, acronym, agency, gender, MAge, Phase1, AgencyClass, StudyType, StudyDesign, SecID, FirstRecv, SDate, ComplDate, LastChangedDate, VerificationDate, FirstRecvResultDate, FirstRecvResultDispoDate, PCD, Countries, Rm_Countries, IsFDA,Is801):
+
+         writer.writerow( (i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12, i13, i14, i15, i16, i17, i18, i19, i20, i21, i22, i23, i24, i25,i26, i27,i28) )
+finally:
+    f.close()
+
+# Writing faulty ID file:
